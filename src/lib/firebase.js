673dc4b27a2d4c4394b1920f8a94c8aa0ba3cd31
Mahammi <muhammadabdulqadir03@gmail.com>
2025-03-08 16:12:19 +0300

    edited

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyDb4kFdLS_mL72wbISNwNGvyMu37kEyiVY",
+    authDomain: "qurban-tracking.firebaseapp.com",
+    projectId: "qurban-tracking",
+    storageBucket: "qurban-tracking.firebasestorage.app",
+    messagingSenderId: "1008182360877",
+    appId: "1:1008182360877:web:9c8cb246c04d27f12731a4",
+    measurementId: "G-26YL2160CC"
 };
 
 const app = initializeApp(firebaseConfig);
